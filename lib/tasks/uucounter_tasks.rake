ed269d76f50d5b4eba1c3275f5ec11413231fcb3
timakin <timaki.st@gmail.com>
2016-05-04 16:52:22 +0900

    remove all

--- lib/tasks/uucounter_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :uucounter do
-#   # Task goes here
-# end
--- test/uucounter_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UucounterTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Uucounter
-  end
-end
